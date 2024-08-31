c88a10a9d544e767dd15cf17844b183041af05a5
Hailey Pan <haileyrpan@gmail.com>
2024-08-31 11:39:09 -0400

    wasd

--- photoData.js
+++ photoData.js
@@ -6,10 +6,25 @@ var photoData = [
         month: 'June',
         year: 2024,
         photos: [
-            '/photos/1_1.JPG',
-            '/photos/1_2.JPG',
-            '/photos/1_3.JPG'
-        ],
+            '/photos/1/1.JPG',
+            '/photos/1/2.JPG',
+            '/photos/1/3.JPG',
+            '/photos/1/4.JPG',
+            '/photos/1/5.JPG',
+            '/photos/1/6.JPG',
+            '/photos/1/7.JPG',
+            '/photos/1/8.JPG',
+            '/photos/1/9.JPG',
+            '/photos/1/10.JPG',
+            '/photos/1/11.JPG',
+            '/photos/1/12.JPG',
+            '/photos/1/13.JPG',
+            '/photos/1/14.JPG',
+            '/photos/1/15.JPG',
+            '/photos/1/16.JPG',
+            '/photos/1/17.JPG',
+            '/photos/1/18.JPG'
+            ],
         description: 'Burke Lake Park, 9:06 PM'
     },
     {
@@ -19,9 +34,23 @@ var photoData = [
         month: 'August',
         year: 2024,
         photos: [
-            '/photos/2_1.JPG',
-            '/photos/2_2.JPG',
-            '/photos/2_3.JPG'
+            '/photos/2/1.JPG',
+            '/photos/2/2.JPG',
+            '/photos/2/3.JPG',
+            '/photos/2/4.JPG',
+            '/photos/2/5.JPG',
+            '/photos/2/6.JPG',
+            '/photos/2/7.JPG',
+            '/photos/2/8.JPG',
+            '/photos/2/9.JPG',
+            '/photos/2/10.JPG',
+            '/photos/2/11.JPG',
+            '/photos/2/12.JPG',
+            '/photos/2/13.JPG',
+            '/photos/2/14.JPG',
+            '/photos/2/15.JPG',
+            '/photos/2/16.JPG',
+            '/photos/2/17.JPG'
         ],
         description: 'Fountainhead Regional Park, 6:06 PM'
     },
@@ -32,36 +61,27 @@ var photoData = [
         month: 'August',
         year: 2024,
         photos: [
-            '/photos/3_1.JPG',
-            '/photos/3_2.JPG'
+            '/photos/3/1.JPG',
+            '/photos/3/2.JPG',
+            '/photos/3/3.JPG',
+            '/photos/3/4.JPG',
+            '/photos/3/5.JPG',
+            '/photos/3/6.JPG',
+            '/photos/3/7.JPG',
+            '/photos/3/8.JPG',
+            '/photos/3/9.JPG',
+            '/photos/3/10.JPG',
+            '/photos/3/11.JPG',
+            '/photos/3/12.JPG',
+            '/photos/3/13.JPG',
+            '/photos/3/14.JPG',
+            '/photos/3/15.JPG',
+            '/photos/3/16.JPG',
+            '/photos/3/17.JPG',
+            '/photos/3/18.JPG',
+            '/photos/3/19.JPG',
+            '/photos/3/20.JPG'
         ],
         description: 'Burke Lake Park, 9:06 PM'
     },
-    {
-        id: 4,
-        lat: 38.60755788888889,
-        lng: -77.26645125,
-        month: 'September',
-        year: 2024,
-        photos: [
-            '/photos/3_1.JPG',
-            '/photos/3_2.JPG'
-        ],
-        description: 'Burke Lake Park, 9:06 PM'
-    },
-    {
-        id: 5,
-        lat: 38.72045927777778,
-        lng: -77.33891588888888,
-        month: 'November',
-        year: 2024,
-        photos: [
-            '/photos/2_1.JPG',
-            '/photos/2_2.JPG',
-            '/photos/2_3.JPG'
-        ],
-        description: 'Fountainhead Regional Park, 6:06 PM'
-    },
-
-    // Add more photo objects as needed
 ];
